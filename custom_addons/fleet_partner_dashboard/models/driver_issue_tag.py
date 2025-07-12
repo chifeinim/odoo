8e217ab79137164517a1f06f7d34bbf50f9ea5ef
chifeinim <chifeinim@gmail.com>
2025-07-13 00:43:59 +0100

    add logic to prevent deletion of used issue tags

--- custom_addons/fleet_partner_dashboard/models/driver_issue_tag.py
+++ custom_addons/fleet_partner_dashboard/models/driver_issue_tag.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class FleetDriverIssueTag(models.Model):
     _name = 'x_fleet_driver_issue_tag'
@@ -6,3 +7,10 @@ class FleetDriverIssueTag(models.Model):
 
     name = fields.Char(string="Tag Name", required=True)
     color = fields.Integer(string="Color Index")
+    issue_ids = fields.Many2many('x_fleet_driver_issue', string="Issues", inverse_name='tag_ids')
+    
+    def unlink(self):
+        for tag in self:
+            if tag.issue_ids:
+                raise UserError("You cannot delete a tag that is linked to issues.")
+        return super().unlink()
