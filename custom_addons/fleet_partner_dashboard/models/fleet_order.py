8c298e6b76ec5a228ed5c7166a6975adf2cdffa2
chifeinim <chifeinim@gmail.com>
2025-07-16 02:03:41 +0100

    fix relation between orders and drivers

--- custom_addons/fleet_partner_dashboard/models/fleet_order.py
+++ custom_addons/fleet_partner_dashboard/models/fleet_order.py
@@ -22,8 +22,8 @@ class FleetOrder(models.Model):
     cancellation_description = fields.Char(string="Cancellation Description") #cancellation_description
     pickup_address = fields.Char(string="Pickup Address") #pickup_address
     price = fields.Float(string="Price") #price, in Angolan Kwanzas (AOA)
-    driver = fields.Many2one('x_fleet_driver', string="Driver") #Contextual link to a driver
-    driver_id = fields.Char(string="Yango Driver ID") #driver_id
+    driver_id = fields.Many2one('x_fleet_driver', string="Driver", ondelete='cascade') #Contextual link to a driver
+    yango_driver_id = fields.Char(string="Yango Driver ID") #driver_id
     driver_name = fields.Char(string="Driver Name") #driver_name
     pick_latitude = fields.Float(string="Pickup Latitude") #pickup_latitude
     pick_longitude = fields.Float(string="Pickup Longitude") #pickup_longitude
