b305f39875028ffe2fcba85eb014f44863e40ffd
chifeinim <chifeinim@gmail.com>
2025-07-20 04:36:49 +0100

    add kpi parameters to product type to allow for calculation of on-the-road score

--- custom_addons/fleet_partner_dashboard/models/product_type.py
+++ custom_addons/fleet_partner_dashboard/models/product_type.py
@@ -6,5 +6,13 @@ class FleetProductType(models.Model):
 
     name = fields.Char(string="Product Name", required=True) #Anda Piloto Normal - 22k, etc
     description = fields.Text(string="Description")
+    kpi_type = fields.Selection([
+        ('avg_hours_online', 'Avg. Hours Online per day'),
+        ('avg_trips_completed', 'Avg. Trips Completed per day'),
+        ('avg_cash', 'Avg. Cash per day'),
+        ('none', 'None'),
+    ], default='none', string="KPI Type")
+    lower_kpi = fields.Float(string="Lower KPI bound")
+    upper_kpi = fields.Float(string="Upper KPI bound")
     active = fields.Boolean(default=True)
     driver_ids = fields.One2many('x_fleet_driver', 'product_type_id', string="Drivers") #Contextual link to drivers
