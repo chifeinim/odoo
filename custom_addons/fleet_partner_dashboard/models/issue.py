dd78e253eec70107b2958f05dfd3f59902d6060c
chifeinim <chifeinim@gmail.com>
2025-07-28 21:41:58 +0100

    add supabase to requirements and remove readonly from issue id

--- custom_addons/fleet_partner_dashboard/models/issue.py
+++ custom_addons/fleet_partner_dashboard/models/issue.py
@@ -7,7 +7,7 @@ class FleetIssue(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _order = 'date_reported desc'
 
-    name = fields.Char(string="Issue ID", copy=False, readonly=True, default='New')
+    name = fields.Char(string="Issue ID", copy=False, default='New')
     date_reported = fields.Datetime(string="Reported On", default=fields.Datetime.now)
     driver_id = fields.Many2one('x_fleet_driver', string="Driver", required=True, ondelete='cascade')
 
