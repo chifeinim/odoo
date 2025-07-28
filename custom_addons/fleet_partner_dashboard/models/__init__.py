2ca9114f5de392061c2efc1d071486b86b9ef16a
chifeinim <chifeinim@gmail.com>
2025-07-28 01:09:46 +0100

    remove all old issue and issue tags references

--- custom_addons/fleet_partner_dashboard/models/__init__.py
+++ custom_addons/fleet_partner_dashboard/models/__init__.py
@@ -1,7 +1,5 @@
 from . import fleet_driver
 from . import fleet_order
-from . import fleet_driver_issue
 from . import product_type
-from . import driver_issue_tag
 from . import driver_supply_hours
 from . import issue
