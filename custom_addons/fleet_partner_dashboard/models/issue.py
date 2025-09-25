3445066ffbd03ca2b9edd89102a860fc8bd7aa9f
chifeinim <chifeinim@gmail.com>
2025-09-25 12:21:19 +0100

    rename note label to Reporter Note

--- custom_addons/fleet_partner_dashboard/models/issue.py
+++ custom_addons/fleet_partner_dashboard/models/issue.py
@@ -34,7 +34,7 @@ class FleetIssue(models.Model):
 
     # NEW: boolean instead of selection
     can_work = fields.Boolean(string="Can Work", default=True, tracking=True)
-    note = fields.Char(string="Note", tracking=True)
+    note = fields.Char(string="Reporter Note", tracking=True)
     can_work_label = fields.Char(string="Can Work", compute="_compute_can_work_label")
     
     ext_attachment_ids = fields.One2many('x_issue_attachment', 'issue_id', string='Media')
