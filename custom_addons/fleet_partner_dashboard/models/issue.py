7b4ac284953773c23f377e46e6e645cd9cb211c6
chifeinim <chifeinim@gmail.com>
2025-12-09 10:57:48 +0000

    fix syntax error and update anda

--- custom_addons/fleet_partner_dashboard/models/issue.py
+++ custom_addons/fleet_partner_dashboard/models/issue.py
@@ -31,10 +31,10 @@ class FleetIssue(models.Model):
         search="_search_sub_sub_category_label")
 
     status   = fields.Selection([('unresolved','Not Started'),
-                                 ('resolved','Resolved')
-                                 ('requires_follow_up_call','Requires Follow-Up Call')
-                                 ('invited_to_office','Invited To Office')
-                                 ('invited_to_workshop','Invited To Workshop')
+                                 ('resolved','Resolved'),
+                                 ('requires_follow_up_call','Requires Follow-Up Call'),
+                                 ('invited_to_office','Invited To Office'),
+                                 ('invited_to_workshop','Invited To Workshop'),
                                  ('unresponsive','Unresponsive')],
                                 default='unresolved', tracking=True)
 
